0733ba421d5a42b85148670b7c637c457cfcd966
Ayue9527 <846295837@qq.com>
2022-03-20 23:27:06 +0800

    第六次提交：     直接在ＢＳＴＳｅｔ里修改好了。。

--- Set/src/BSTSet.java
+++ Set/src/BSTSet.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BSTSet<E extends Comparable<E>> implements Set<E> {
     private BST<E> bst;
 
@@ -31,5 +33,26 @@ public class BSTSet<E extends Comparable<E>> implements Set<E> {
         bst.remove(e);
     }
 
+    public static void main(String[] args) {
 
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> words1 = new ArrayList<>();
+        if (FileOperation.readFile("pride-and-prejudice.txt", words1)) {
+            System.out.println("Total words: " + words1.size());
+            BSTSet<String> set1 = new BSTSet<>();
+            for (String word : words1)
+                set1.add(word);
+            System.out.println("Total different words: " + set1.getSize());
+        }
+
+        System.out.println("a-tale-of-two-cities");
+        ArrayList<String> words2 = new ArrayList<>();
+        if (FileOperation.readFile("a-tale-of-two-cities.txt", words1)) {
+            System.out.println("Total words: " + words2.size());
+            BSTSet<String> set2 = new BSTSet<>();
+            for (String word : words2)
+                set2.add(word);
+            System.out.println("Total different words: " + set2.getSize());
+        }
+    }
 }
